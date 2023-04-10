91f34ae33abf39c964feb30f4f42fa262ae43a54
juser06 <jose.a.vereda@gmail.com>
2023-04-10 20:13:56 -0300

    cambio de idea en el xmal

--- Note_App_with_Database/MainWindow.xaml.cs
+++ Note_App_with_Database/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using Note_App_with_Database.forpracticeDataSetTableAdapters;
 
 namespace Note_App_with_Database
 {
@@ -20,9 +23,40 @@ namespace Note_App_with_Database
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinqToSqlDataClassesDataContext dataContext;
+  
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["Note_App_with_Database.Properties.Settings.forpracticeConnectionString"].ConnectionString;
+            dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+            MostrarNotas();
+        }
+
+        private void MostrarNotas()
+        {
+            ListaDeTitulosPendientes.ItemsSource = null;
+            ListaDeTitulosPendientes.ItemsSource = dataContext.Notas;
+        }
+
+        private void Guardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (Titulo_Box.Text == null || Titulo_Box.Text.Trim() == "")
+            {
+                return;
+            }
+            Nota nota = new Nota();
+            nota.Titulo = Titulo_Box.Text;
+            nota.Nota1 = Nota_box.Text;
+            nota.Hora = DateTime.Now.ToString();
+            nota.Pendiente_Archivado = true;
+            dataContext.Notas.InsertOnSubmit(nota);
+            dataContext.SubmitChanges();
+            ListaDeTitulosPendientes.Items.Refresh();
+            Titulo_Box.Clear();
+            Nota_box.Clear();
         }
     }
 }
